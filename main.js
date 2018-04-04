acea70384eb5ed11cb9cf94d3cacfb41608fc2ef
Antonio Reuter <antonio.reuter@philips.com>
2018-04-04 11:30:57 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({});
+
+    mainWindow.loadUrl(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+})
